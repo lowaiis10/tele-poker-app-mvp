6c51465e01546316b1c03a7c9c84d36eee84347b
tobirama21 <lowaiis10ss@gmail.com>
2024-12-25 23:20:42 +0800

    Add updates to solana-project

--- poker/bot/utils.py
+++
@@ -1,7 +0,0 @@
-def format_players(players):
-    """Format player list for output."""
-    return "\n".join([f"@{player[1]}" for player in players])
-
-def calculate_pot(bets):
-    """Calculate the total pot from bets."""
-    return sum(bets.values())
